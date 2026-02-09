7f865a58842231d52c77badd20e9488ad4ef3168
Krish V <krishh.v777@gmail.com>
2026-02-09 23:36:00 +0530

    fix: cors issue

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,14 +1,18 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],  
     
     'allowed_methods' => ['*'],
     
     'allowed_origins' => [
-        'http://localhost:80800',
+        'http://localhost:8081',
+        'http://127.0.0.1:8080',
+        'http://127.0.0.1:8081',
         'https://360astra.io',
         'https://www.360astra.io',
+        'https://api.360astra.io/api',
+        'https://api.360astra.io',
         env('FRONTEND_URL', 'http://localhost:8080')
     ],
     
